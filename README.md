9c02b4adbae343ce415ba1443d899e0acb32efaf
Cat Boxer <thecatboxer@gmail.com>
2021-01-07 16:35:51 -0800

    added create new song form

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Lyrical-GraphQL
-Starter project from a GraphQL course on Udemy.com
+# Songs-GraphQL
+
+Starter project from a GraphQL course
--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,25 +1,65 @@
-import React from 'react'
 
-const SongCreate = () => {
-  // const [title, setTitle] = useState("")
-  // handleChange=(event)=>{
-  //   console.log(
-  //  event.target.value
-  //   )
-  // }
+// import React, { useState } from 'react';
+
+import React, {Component}  from 'react'
+import gql from 'graphql-tag'
+import {graphql} from 'react-apollo'
+class SongCreate extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={title:''}
+  }
+onSubmit(event){
+  event.preventDefault();
+this.props.mutate({
+  variables: {
+    title: this.state.title
+  }
+})
+}
+  render(){
   return (
     <div>
       <h3>Create A New Song</h3>
-      <form action="">
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label htmlFor="title">Song Title:</label>
         <input 
-        // onChange={handleChange}
-        type="text" 
-        name="title"
-        placeholder="Song"/>
+        onChange={(e)=>this.setState({title: e.target.value})}
+        value={this.state.title}
+        />
       </form>
-      
     </div>
   )
+}}
+const mutation = gql `
+mutation AddSong($title: String){ 
+addSong(title:$title) {
+  id
+  title
+	}
 }
-export default SongCreate
+`
+export default graphql(mutation)(SongCreate);
+
+// function SongCreate () {
+
+//   const [title, setTitle] = useState("");
+
+//   return (
+//     <div>
+//       <h3>Create A New Song</h3>
+//       <form>
+//         <label htmlFor="title">Song Title:</label>
+//         <input 
+//         onChange={(e)=>{setTitle(e.target.value)}}
+//         // type="text" 
+//         // name="title"
+//         // placeholder="Enter Song Title"
+//         value={title}
+//         />
+//       </form>
+//     </div>
+//   )
+// }
+// export default SongCreate;
