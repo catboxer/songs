b47750105855fc2d0d3cb9cdb093469d94b039dc
Cat Boxer <thecatboxer@gmail.com>
2021-01-08 11:56:03 -0800

    added mutation to add songs

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -4,6 +4,9 @@
 import React, {Component}  from 'react'
 import gql from 'graphql-tag'
 import {graphql} from 'react-apollo'
+import {Link, hashHistory} from 'react-router'
+import query from '../queries/fetchSongs'
+
 class SongCreate extends Component{
 
   constructor(props){
@@ -15,12 +18,18 @@ onSubmit(event){
 this.props.mutate({
   variables: {
     title: this.state.title
-  }
-})
+  },
+  refetchQueries:[{query:query}]
+}).then(()=> hashHistory.push('/'))
+.catch((err) => console.log(err))
 }
   render(){
   return (
     <div>
+              <Link 
+        to="/">
+          Back
+        </Link>
       <h3>Create A New Song</h3>
       <form onSubmit={this.onSubmit.bind(this)}>
         <label htmlFor="title">Song Title:</label>
